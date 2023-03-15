3139408d995cc011d02bc3aec966350765210398
PastelPossum <iggyskywalker@gmail.com>
2023-03-15 09:35:28 +0100

    add fetch for pokemon cards on front page

---
+++ cards-fetch.js
@@ -0,0 +1,26 @@
+const pokemonWrap = document.querySelector(".pokemon-wrapper")
+
+const pokemonNames = ["sobble", "kecleon", "phantump","banette", "bronzor", "kricketune"]
+
+pokemonNames.forEach(name => {
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then(res => res.json())
+        .then(data =>{
+
+            pokemonWrap.innerHTML += `
+            
+            <div class="cards">
+
+            <h2>${data.name}</h2>
+            <img src="${data.sprites.front_default}" alt="">
+
+            <p><a href="#">More</a></p>
+
+            </div>
+            
+            `
+
+        })
+
+})
