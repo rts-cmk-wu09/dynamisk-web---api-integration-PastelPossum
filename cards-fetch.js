4a8a2d40373cfb547108a1db45d242395da495f7
PastelPossum <iggyskywalker@gmail.com>
2023-03-21 10:38:10 +0100

    add axios to replace fetch

--- cards-fetch.js
+++ cards-fetch.js
@@ -1,37 +1,20 @@
+import axios from "./node_modules/axios/dist/esm/axios.js"
+
 const pokemonWrap = document.querySelector(".pokemon-wrapper")
 
-const pokemonNames = [
-"sobble", 
-"kecleon", 
-"phantump",
-"banette", 
-"bronzor", 
-"kricketune",
-"impidimp",
-"tatsugiri",
-"rowlet"
+axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+.then(res => {
 
-]
+    res.data.results.forEach(pokemon => {
 
-pokemonNames.forEach(name => {
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-        .then(res => res.json())
-        .then(data =>{
-
-            pokemonWrap.innerHTML += `
+        pokemonWrap.innerHTML += `
             
-            <div class="cards">
+      <div class="cards">
+      <h2>${pokemon.name}</h2>
+      <p><a href="details.html?name=${pokemon.name}">More</a></p>
+      </div>
+      `
 
-            <h2>${data.name}</h2>
-            <img src="${data.sprites.front_default}" alt="">
-
-            <p><a href="details.html?name=${data.name}">More</a></p>
-
-            </div>
-            
-            `
-
-        })
+    })
 
 })
